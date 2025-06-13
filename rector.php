38650a6b40b3b592a93d9efc9d5172284610ed76
Alexander Dimitrov <alexander.dimitrov@slr-holiday-service.bg>
2025-06-13 11:30:13 +0300

    fix: accessed before initialization

--- rector.php
+++ rector.php
@@ -3,10 +3,7 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Doctrine\Set\DoctrineSetList;
 use Rector\Naming\Rector\Class_\RenamePropertyToMatchTypeRector;
-use Rector\Nette\Set\NetteSetList;
-use Rector\Php80\Rector\FunctionLike\UnionTypesRector;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
 
@@ -17,21 +14,13 @@ return static function (RectorConfig $rectorConfig): void {
 
     $rectorConfig->skip([
         RenamePropertyToMatchTypeRector::class => [__DIR__ . '/tests/ORM/'],
-
-        UnionTypesRector::class => [
-            // to keep BC return types
-            __DIR__ . '/src/Contract/Entity',
-            'src/Model/*/*Trait.php',
-        ],
     ]);
 
     // doctrine annotations to attributes
     $rectorConfig->sets([
-        DoctrineSetList::DOCTRINE_ORM_29,
         SetList::DEAD_CODE,
         SetList::CODE_QUALITY,
         SetList::CODING_STYLE,
-        NetteSetList::NETTE_CODE_QUALITY,
         SetList::NAMING,
         LevelSetList::UP_TO_PHP_80,
     ]);
